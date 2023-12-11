a99bbf648c61a49549a1a76319170b954b9a5840
Влад Книгин <vlaknig@gmail.com>
2023-12-12 02:02:36 +0300

    установка EntityFrameworkCore и создание класса DataContext

---
+++ Telbat/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Telbat.Data
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=DbTelbat;Trusted_Connection=True;");
+        }
+
+    }
+}
