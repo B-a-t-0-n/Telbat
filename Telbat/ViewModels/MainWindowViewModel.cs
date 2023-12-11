3aad16918c1f3756373d02039c19b259a908046e
Влад Книгин <vlaknig@gmail.com>
2023-12-12 01:09:45 +0300

    отчистка мусора и тестирование представления главного окна

--- Telbat/ViewModels/MainWindowViewModel.cs
+++ Telbat/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Telbat.ViewModels.Base;
 
 namespace Telbat.ViewModels
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        #region Заголовок окна
+
+        private string _title = "Гланое окно";
+
+        /// <summary>
+        /// заголовок окна
+        /// </summary>
+        public string Title 
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+
+        #endregion
     }
 }
